4d7bc4a75871f3466ee447d799c17253a6954b80
scottcl68 <clscott@ucsb.edu>
2022-04-08 07:24:17 -0600

    first contact listing; scrolling; non functional

--- staq-address-book/src/App.tsx
+++ staq-address-book/src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { render } from "react-dom";
+import {firstNames, surNames as lastNames} from './names'
+import {ContactCard, Address} from './classes';
 import './App.css';
 
+import ReactTable from "react-table";
+// import "react-table/react-table.css";
+
+
+//initialize contact container
+let contacts = new Map<string, ContactCard>();
+
+//generate 25 new contact cards with first and sur names only
+for (let i = 0; i < 25; i++) {
+  let firstName = firstNames[Math.floor(Math.random() * firstNames.length)]
+  let lastName = lastNames[Math.floor(Math.random() * lastNames.length)]
+  let contact = new ContactCard();
+  contact.firstName = firstName;
+  contact.lastName = lastName;
+  //add new random contact to contact container
+  contacts.set(contact.key, contact)
+}
+
+
+function addNewContact(newContact: ContactCard){
+  contacts.set(newContact.key, newContact)
+}
+
+
+
 function App() {
+  const [users, setUsers] = useState(Array.from(contacts.values()));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className='page-header'>
+            <h1>STAQ ADDRESS BOOK</h1>
+        </div>
+        <div className="contact-list">
+            <h3 className="contact-list-header">Contact List</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users && users.map(user =>
+                        <tr key={user.key}>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ staq-address-book/src/classes.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {v4 as uuidv4} from 'uuid'
+
+export class Address {
+    street?: string;
+    unit?: string;
+    city?: string;
+    state?: string;
+    zip?: number;
+  }
+  
+  export class ContactCard {
+    key: string;
+    firstName?: string;
+    lastName?: string;
+    address?: Address;
+    email?: string;
+    phone?: number;
+    notes?: string;
+  
+    constructor () {
+      this.key = uuidv4();
+    }
+  }
